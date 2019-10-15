836f74247925e46ab23c1220df0bf9020e62d15d
DuiNaru <kangkimu@gmail.com>
2019-10-16 01:05:19 +0900

    jekyll build

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 chcp 65001
 rm -r _site/*
-git clone -b master `github.com/DuiNaru/duinaru.github.io.git` _site
+git clone -b master `git://github.com/DuiNaru/duinaru.github.io.git` _site
 bundle exec jekyll build
 cd _site
 git add -A
