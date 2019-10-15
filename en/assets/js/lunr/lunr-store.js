08b5056825e11f6d3e5d16847490596b22ef2077
DuiNaru <kangkimu@gmail.com>
2019-10-16 01:07:18 +0900

    jekyll build

---
+++ en/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "This blog is under constructing",
+        "excerpt":"Will open soon  ","categories": [],
+        "tags": [],
+        "url": "http://duinaru.github.io/2019/10/13/blog-intro",
+        "teaser":null}]
---
+++ ja/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "このサイトは現在、工事中です。",
+        "excerpt":"新しい内容でお伺いします。  ","categories": [],
+        "tags": [],
+        "url": "http://duinaru.github.io/2019/10/13/blog-intro",
+        "teaser":null}]
