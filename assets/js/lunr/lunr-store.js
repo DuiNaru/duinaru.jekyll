94c4de9d3ea5a6f9edd1e7ab23fc14a03ab25f5e
DuiNaru <kangkimu@gmail.com>
2019-10-23 02:04:45 +0900

    jekyll build

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "이 블로그는 현재 공사중 입니다.",
-        "excerpt":"곧 새로운 글로 찾아뵙겠습니다.  ","categories": [],
+        "title": "Github Pages로 블로그 만들기",
+        "excerpt":"자신만의 홈페이지를 가지고 싶다는 생각을 해본 적이 있는 분들이 있을 것이다. 블로그 서비스를 이용하여 블로그를 운영해 볼 수 도 있고, 웹 호스팅이나 직접 구축한 서버를 이용하여 웹 서비스를 만들어서 운영해 볼 수도 있을 것이다. 필자도 이러한 생각을 하던 중에 Github Pages라는 것을 알게 되어서 만들어 보게 되었다. Github Pages Github...","categories": [],
         "tags": [],
-        "url": "https://duinaru.github.io/2019/10/13/blog-intro",
+        "url": "https://duinaru.github.io/2019/10/23/make-blog-with-github-pages",
         "teaser":null}]
--- en/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "This blog is under constructing",
-        "excerpt":"Will open soon  ","categories": [],
-        "tags": [],
-        "url": "https://duinaru.github.io/2019/10/13/blog-intro",
-        "teaser":null}]
