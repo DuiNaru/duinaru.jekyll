1e31e7c015e2d6e69420fe9a9045c84fb3b92583
DuiNaru <kangkimu@gmail.com>
2019-10-24 00:20:26 +0900

    jekyll build

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "개발과 경험이 함께 하는 블로그",
         "excerpt":"평소에 생각하던 이것저것을 적어볼까 하다가 블로그를 만들게 되었습니다. 블로그를 운영한다면 블로그 서비스를 이용하는 편이 간단하고 편할 듯 했지만, github.io 라는 도메인이 눈에 들어오더군요. 그래서 github pages로 만들게 되었습니다. 준비물 github github pages를 서비스할 repogitory를 만들었습니다. jekyll github pages는 jekyll을 지원합니다. jekyll로 된 소스를 repogitory에 올려 두는 것 만으로도 빌드-배포를 해주죠....","categories": [],
         "tags": [],
-        "url": "https://duinaru.github.io/2019/10/23/make-blog-with-github-pages",
+        "url": "https://duinaru.github.io/2019/10/23/blog-with-development-and-experience",
         "teaser":null}]
--- ja/assets/js/lunr/lunr-store.js
+++ ja/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "開発と経験があるブログ",
         "excerpt":"普段、考えていた色々なものを書こうとブログを作ることにしました。 ブログを運営しようとしたらブログサービスを利用する方が簡単で便利そうでしたが、github.ioというドメインが目立ちました。 それで、github pagesで作ることにしました。 必要な物 github github pagesをサービスするrepogitoryを作りました。 jekyll github pagesはjekyllをサポートします。 jekyllのソースをrepogitoryに置いておくだけでビルド-デプロイをしてくれます。 ruby jekyllで作るにはrubyが必要です。 buildをしたり、pluginをインストールして機能を追加することができます。 作る ruby jekyllのbuildと色々なpluginを利用するためにインストールしました。 jekyllの公式ホームページに手順があり、便利でした。 マルチランゲージ 二つ以上の言語で運営することを目指してしていまして、マルチランゲージのサポートのためpolygot pluginを使用しました。 このプラグインはgithub pagesで公式的にサポートするプラグインではありませんので、単純にrepogitoryにソースを置いておくことだけでビルドはできません。 しかし、方法はありました。 テーマ テーマを選ぶことに時間がかなりかかりました。 これにしようかあれにしようかとしたら時間があっという間に過ぎました。 結局はMinimal Mistakesにすることにしました。 しかし、ここで問題が出ました。 buildの時、エラーが出ます。 polygot pluginが動かないです。 問題解決 windows環境でbuildの時、文字関連のエラー解決 rubyをwindowsの環境で使おうとしたら出るエラーでした。 vscodeからcmdでビルドしていたので、chcp 65001 でUTF-8に変えて解決しました。 また、jekyllをエラーが出ないバージョンに変更する方法もありますが、下記の問題でバージョンを制限しました。 Minimal Mistakesとpolygot pluginを共に使用 ウェブページを正常に作れない問題が出ました。 繰り返して試し、次のように解決できました。 jekyllバージョン制限 jekyllのバージョンを3.8.5としたら、polygot pluginと共に動きました。 polyglot...","categories": [],
         "tags": [],
-        "url": "https://duinaru.github.io/2019/10/23/make-blog-with-github-pages",
+        "url": "https://duinaru.github.io/2019/10/23/blog-with-development-and-experience",
         "teaser":null}]
