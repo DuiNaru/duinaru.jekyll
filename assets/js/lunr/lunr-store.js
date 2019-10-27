eef9934eb75db899ccfb0bd452a8e5481b79ff12
DuiNaru <kangkimu@gmail.com>
2019-10-27 21:37:16 +0900

    jekyll build

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"평소에 생각하던 이것저것을 적어볼까 하다가 블로그를 만들게 되었습니다. 블로그를 운영한다면 블로그 서비스를 이용하는 편이 간단하고 편할 듯 했지만, github.io 라는 도메인이 눈에 들어오더군요. 그래서 github pages로 만들게 되었습니다. 준비물 github github pages를 서비스할 repository를 만들었습니다. jekyll github pages는 jekyll을 지원합니다. jekyll로 만든 소스를 repository에 올려 두는 것 만으로도 빌드-배포를 해주죠....","categories": [],
         "tags": [],
         "url": "https://duinaru.github.io/2019/10/23/blog-with-development-and-experience",
+        "teaser":null},{
+        "title": "GitHub Pages에서 지원하지 않는 플러그인 쓰기",
+        "excerpt":"jekyll을 공식적으로 지원하는 GitHub Pages이지만, 모든 jekyll의 플러그인까지 지원하는 것은 아닙니다. GitHub Pages는 지원하는 플러그인이 제한 되어있습니다. 이 이외의 플러그인은 GitHub Pages에서 jekyll을 build할때, 제한되어 적용되지 않습니다. 그럼, GitHub Pages에서 jekyll로 웹 페이지 서비스를 만들 때는 일부 플러그인만 사용해야 하는 것 일까요? 반은 맞고 반은 틀립니다. 알아보기 전에, 먼저 어떻게...","categories": [],
+        "tags": [],
+        "url": "https://duinaru.github.io/2019/10/26/how-to-make-a-github-pages-work-with-an-unsupported-plugin",
         "teaser":null}]
